f346f1403f334d65fae3541f29af1c564bfb7364
nmakarin <n-makarin@mail.ru>
2019-07-08 17:08:58 +0300

    Add pages/index.vue Add .nuxt/ in .gitignore

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page page-index">
+    yo
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
