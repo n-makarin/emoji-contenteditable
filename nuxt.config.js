09e4d51715a34101c802d83865a1fbdb6a33aafe
nmakarin <n-makarin@mail.ru>
2019-07-09 11:13:48 +0300

    Add eslint config in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  mode: 'universal'
+  mode: 'universal',
+
+  build: {
+    vendor: [
+    ],
+    /*
+    ** You can extend webpack config here
+    */
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['> 5%']
+      })
+    ],
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: { fix: true }
+        })
+      }
+    }
+  }
 }
