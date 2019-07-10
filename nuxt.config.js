dcf2fadb255002c2211f9b05d5f70537ade6de10
nmakarin <n-makarin@mail.ru>
2019-07-10 09:21:50 +0300

    Replace browserslist to package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,6 @@ module.exports = {
   build: {
     vendor: [
     ],
-    /*
-    ** You can extend webpack config here
-    */
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['> 5%']
-      })
-    ],
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
