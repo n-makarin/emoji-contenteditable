ca9fbe8abdd7781c446ab7501c526ac7ee56a2e9
nmakarin <n-makarin@mail.ru>
2019-07-10 12:43:06 +0300

    Create utils/caret.js with getPosition method

---
+++ utils/caret.js
@@ -0,0 +1,31 @@
+export default {
+  /**
+   * Get contentEditable caret's position
+   * @param {Object} editableDiv Html reference
+   */
+  getPosition (editableDiv) {
+    let caretPos = 0
+    let sel = null
+    let range = null
+    if (window.getSelection) {
+      sel = window.getSelection()
+      if (sel.rangeCount) {
+        range = sel.getRangeAt(0)
+        if (range.commonAncestorContainer.parentNode === editableDiv) {
+          caretPos = range.endOffset
+        }
+      }
+    } else if (document.selection && document.selection.createRange) {
+      range = document.selection.createRange()
+      if (range.parentElement() === editableDiv) {
+        const tempEl = document.createElement('span')
+        editableDiv.insertBefore(tempEl, editableDiv.firstChild)
+        const tempRange = range.duplicate()
+        tempRange.moveToElementText(tempEl)
+        tempRange.setEndPoint('EndToEnd', range)
+        caretPos = tempRange.text.length
+      }
+    }
+    return caretPos
+  }
+}
