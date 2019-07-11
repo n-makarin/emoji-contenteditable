f2aaae079ffb8d28a6c1359ea838b5eae6ec9361
nmakarin <n-makarin@mail.ru>
2019-07-11 16:08:43 +0300

    Change styles in pages/index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="page page-index">
     <h1>Contenteditable component with twitter emoji picker</h1>
-    <section>
-      <div>
-        <p>contenteditable</p>
+    <section class="contentEditable">
+      <div class="contentEditable__item">
+        <p>Contenteditable</p>
         <content-editable
           v-model="contentEditableText"
           :emoji="selectedEmoji"
@@ -15,8 +15,8 @@
           modal-position="bottom-right"
         />
       </div>
-      <div>
-        <p>text output</p>
+      <div class="contentEditable__item">
+        <p>Contenteditable output from v-model</p>
         <textarea
           v-model="contentEditableText"
           type="text"
@@ -56,23 +56,43 @@ export default {
   box-sizing: border-box;
 }
 h1 {
-  margin 20px
+  font-size 25px
+  text-align center
+  margin 25px
   color #413737
 }
 .page-index {
+  display flex
+  flex-direction column
+
   section {
     padding 10px 20px
     display flex
-    .emoji-picker {
-      margin 0 15px
+    justify-content center
+
+    p {
+      font-size 15px
+      padding-bottom 10px
     }
+  }
+
+  .contentEditable {
+    flex-direction column
+    margin-left auto
+    margin-right auto
+    padding 40px 100px
+    background #eee
+
+    .emoji-picker {
+      margin 0 5px
+    }
+
     textarea {
       min-width 400px
       min-height 60px
     }
-    p {
-      font-size 14px
-      padding-bottom 10px
+    &__item:first-child {
+      margin-bottom 30px
     }
   }
 }
